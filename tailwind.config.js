a927d6cf047422847f3a2ed3c76a6d6c268af677
AbimanyuBP <70093819+AbimanyuBP@users.noreply.github.com>
2024-07-29 16:35:29 +0700

    Extend tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}", 
   ], 
   theme: { 
-    extend: {}, 
+    extend: {
+      backgroundImage:{
+        'swirly-pattern': "url('assets/svgs/swirly.svg')",
+      },
+      backgroundColor:{
+        'yale-blue': "#043873",
+      },
+    }, 
   }, 
   plugins: [], 
 } 
